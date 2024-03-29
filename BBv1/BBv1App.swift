741eeffacf67fcdd0a7c5caf31126e268fdcbd83
Alan Grenfell <50964947+AlanGrenfell@users.noreply.github.com>
2024-03-29 17:34:01 +0000

    Initial Commit

---
+++ BBv1/BBv1App.swift
@@ -0,0 +1,17 @@
+//
+//  BBv1App.swift
+//  BBv1
+//
+//  Created by Alan Grenfell on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BBv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BBv1UITests/BBv1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BBv1UITestsLaunchTests.swift
+//  BBv1UITests
+//
+//  Created by Alan Grenfell on 29/03/2024.
+//
+
+import XCTest
+
+final class BBv1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
